fcc469acae79a96d7bfeddd2d90140e89ad10f00
Hector Javier Gordin <hectorjdgdev@gmail.com>
2022-12-15 22:18:09 +0000

    Added google analytics and crash

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,31 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:provider/provider.dart';
 
 import 'calculator_home/controller/calculator_controller.dart';
 import 'calculator_home/views/calculator_container.dart';
-import 'calculator_home/views/calculator_home.dart';
 import 'utils/applicationt.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
-void main() {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   MobileAds.instance.initialize();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
   runApp(const MyApp());
 }
 
