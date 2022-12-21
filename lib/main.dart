e1ae36ff335203a4dc812dd8fdb0291944d86f09
Hector Javier Gordin <hectorjdgdev@gmail.com>
2022-12-21 12:46:19 +0000

    Updated Android version for release

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'dart:ui';
 
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:hns_calculator/settings/controller/payments_controller.dart';
 import 'package:provider/provider.dart';
 
 import 'calculator_home/controller/calculator_controller.dart';
@@ -11,10 +13,15 @@ import 'utils/applicationt.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:in_app_purchase_android/in_app_purchase_android.dart';
+
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   MobileAds.instance.initialize();
+  if (defaultTargetPlatform == TargetPlatform.android) {
+    InAppPurchaseAndroidPlatformAddition.enablePendingPurchases();
+  }
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
